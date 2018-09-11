c952cdc3c7c8f017b82d352e149be3944ff82d0a
Ademir Francisco da Silva <ademir.desilva@trustedshops.de>
2018-09-11 14:27:25 +0200

    Multi container deploy - AWS

---
+++ complex/server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDS_HOST,
+  redisPort: process.env.REDS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+};
